8dc265dd3adee7bce6c208be3aa5098e69935ff4
DODAM <64620695+kimhyunseo@users.noreply.github.com>
2026-02-03 22:41:31 +0900

    wip: 중간 저장

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'package:tasks/core/routes/app_route.dart';
 import 'package:tasks/firebase_options.dart';
 import 'package:tasks/core/theme/app_theme.dart';
@@ -10,7 +9,6 @@ import 'package:tasks/ui/theme_view_model.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await initializeDateFormatting('ko_KR', '');
 
   runApp(const ProviderScope(child: MyApp()));
 }
